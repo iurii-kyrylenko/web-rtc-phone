20934a7973f62087c2aac292d243c2f6fd128510
iurii-kyrylenko <yuriproject@gmail.com>
2021-02-20 12:08:34 +0200

    Configure turn server

--- src/config.js
+++ src/config.js
@@ -1,11 +1,25 @@
+const iceConfig = {
+  iceServers: [{
+      urls: "stun:stun.l.google.com:19302"
+    }, {
+      urls: "turn:numb.viagenie.ca",
+      username: "webrtc@live.com",
+      credential: "muazkh"
+  }],
+  sdpSemantics: "unified-plan"
+};
+
 // export const peerConfig = {
 //   host: window.location.hostname,
 //   port: 9000,
+//   config: iceConfig,
 //   path: '/web-phone',
 //   debug: 2
 // };
 
-export const peerConfig = {};
+export const peerConfig = {
+  config: iceConfig
+};
 
 // export const keys = {
 //   left: "ArrowLeft",
